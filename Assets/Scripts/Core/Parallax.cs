c7b6b226edce91fb0cc24b2fc2dfc797c2f9f27c
villalbaweb <villalbaweb@gmail.com>
2021-01-27 14:55:54 -0600

    Adding Parallax Script

---
+++ Assets/Scripts/Core/Parallax.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Core
+{
+    public class Parallax : MonoBehaviour
+    {
+        // config params
+        [SerializeField] float parallaxEffect = 0.0f;
+
+        // state
+        private Camera m_camera;
+        private float lengthX, lengthY, startposX, startposY;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            m_camera = Camera.main;
+
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+
+            startposX = transform.position.x;
+            startposY = transform.position.y;
+            lengthX = renderer.bounds.size.x;
+            lengthY = renderer.bounds.size.y;
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            ProcessXPosition();
+            ProcessYPosition();
+        }
+
+        void ProcessXPosition()
+        {
+            float temp = (m_camera.transform.position.x * (1 - parallaxEffect));
+            float distance = (m_camera.transform.position.x * parallaxEffect);
+            transform.position = new Vector3(startposX + distance, transform.position.y, transform.position.z);
+            if (temp > startposX + lengthX)
+            {
+                startposX += lengthX;
+            }
+            else if (temp < startposX - lengthX)
+            {
+                startposX -= lengthX;
+            }
+        }
+
+        void ProcessYPosition()
+        {
+            float temp = (m_camera.transform.position.y * (1 - parallaxEffect));
+            float distance = (m_camera.transform.position.y * parallaxEffect);
+            transform.position = new Vector3(transform.position.x, startposY + distance, transform.position.z);
+            if (temp > startposY + lengthY)
+            {
+                startposY += lengthY;
+            }
+            else if (temp < startposY - lengthY)
+            {
+                startposY -= lengthY;
+            }
+        }
+    }
+}
