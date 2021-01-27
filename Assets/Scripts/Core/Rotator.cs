3a167e162696c939c7ae62e46f086cb2c457c3a0
villalbaweb <villalbaweb@gmail.com>
2021-01-27 13:35:08 -0600

    Adding rotating blade prefab, need to tweak

---
+++ Assets/Scripts/Core/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Core
+{
+    public class Rotator : MonoBehaviour
+    {
+        // config params
+        [SerializeField] [Range(-7200f, 7200f)] float rotation = 3600f;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(Vector3.forward, rotation * Time.deltaTime, Space.World);
+        }
+    }
+}
