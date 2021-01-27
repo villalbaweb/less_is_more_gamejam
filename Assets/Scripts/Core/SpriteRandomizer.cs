f4e9f4d8e09fc5b7c3f7aaa05ea4d29b7a94833a
villalbaweb <villalbaweb@gmail.com>
2021-01-26 20:03:34 -0600

    Adding random blood stains

---
+++ Assets/Scripts/Core/SpriteRandomizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Core
+{
+    public class SpriteRandomizer : MonoBehaviour
+    {
+        //config
+        [SerializeField] Sprite[] bloodSprites = null;
+
+        // state
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake() 
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+
+            int randomSpritePos =  Random.Range(0, bloodSprites.Length);
+
+            _spriteRenderer.sprite = bloodSprites[randomSpritePos];    
+        }
+    }
+}
