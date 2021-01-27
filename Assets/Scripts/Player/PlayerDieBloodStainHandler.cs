05a7258b05b30b9e79c4b66200f5446754508894
villalbaweb <villalbaweb@gmail.com>
2021-01-26 19:44:05 -0600

    Adding blodd stain mechanism

---
+++ Assets/Scripts/Player/PlayerDieBloodStainHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Player
+{
+    public class PlayerDieBloodStainHandler : MonoBehaviour
+    {
+        // config
+        [SerializeField] GameObject bloodStain = null;
+
+        public void AddBloddStain()
+        {
+            Instantiate(bloodStain, transform.position, Quaternion.identity);
+        }
+    }
+}
