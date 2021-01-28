375daa9711adcd1fd426e0d7021bbb989686e9f6
villalbaweb <villalbaweb@gmail.com>
2021-01-28 17:51:43 -0600

    Adding SceneLoader object to interact with buttons

---
+++ Assets/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace LessIsMore.Core
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void LoadMainMenu()
+        {
+            SceneManager.LoadScene("Start");
+        }
+
+        public void LoadPlayLevel()
+        {
+            SceneManager.LoadScene("Level");
+        }
+    }
+}
