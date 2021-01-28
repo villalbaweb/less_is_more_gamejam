a6babc056cd193a7d5e3b74ed4cc36586e1e8eca
villalbaweb <villalbaweb@gmail.com>
2021-01-28 08:51:21 -0600

    Adding Particle Systems on pickup

--- Assets/Scripts/Pickup/PickupTakeController.cs
+++ Assets/Scripts/Pickup/PickupTakeController.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace LessIsMore.Pickup
 {
     public class PickupTakeController : MonoBehaviour
     {
+        // config
+        [SerializeField] UnityEvent onPickup = null;
+
         private void OnTriggerEnter2D(Collider2D other) 
         {
-            print("taken...");    
+            onPickup.Invoke();            
         }
     }
 }
