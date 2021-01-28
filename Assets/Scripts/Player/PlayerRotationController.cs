056c4656718579cdcb5fa76c0204b43eab4a989f
villalbaweb <villalbaweb@gmail.com>
2021-01-28 08:05:11 -0600

    Player's body rotates towards mous position

---
+++ Assets/Scripts/Player/PlayerRotationController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Player
+{
+    public class PlayerRotationController : MonoBehaviour
+    {
+        // cache
+        Transform _bodyTransform;
+        PlayerJumpAimController _playerJumpAimController;
+
+
+        void Awake()
+        {
+            _bodyTransform = transform.Find("Body");
+            _playerJumpAimController = GetComponent<PlayerJumpAimController>();
+        }
+
+        void Update()
+        {
+            CalculatePlayerOrientation();
+        }
+
+        private void CalculatePlayerOrientation()
+        {
+            float playerXDirection = _playerJumpAimController.AimDirection.x < 0
+                ? -1
+                : 1;
+            Vector3 playerDirection = new Vector3(playerXDirection, 1, 1);
+            _bodyTransform.localScale = playerDirection;
+
+        }
+    }
+}
