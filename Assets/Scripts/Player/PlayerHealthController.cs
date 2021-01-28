b7f0bc5364f8116bed91c02eceeda6fb29d25b7c
villalbaweb <villalbaweb@gmail.com>
2021-01-28 09:00:29 -0600

    Now the PLayer has a small health system

---
+++ Assets/Scripts/Player/PlayerHealthController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Player
+{
+    public class PlayerHealthController : MonoBehaviour
+    {
+        // config
+        [SerializeField] int maxHealthPoints = 20;
+        [SerializeField] int healthPoints = 20;
+
+        public void IncreaseHealth(int increaseHealthPoints)
+        {
+            healthPoints += increaseHealthPoints;
+
+            if(healthPoints > maxHealthPoints) 
+            {
+                healthPoints = maxHealthPoints;
+            }
+        }
+
+        public void DecreaseHealthPoint()
+        {
+            healthPoints --;
+        }
+    }
+}
