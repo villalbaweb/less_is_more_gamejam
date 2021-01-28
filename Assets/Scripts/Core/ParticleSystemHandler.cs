5f3400a150dcf89c473a84f8d2a39144fadb662e
villalbaweb <villalbaweb@gmail.com>
2021-01-28 08:47:58 -0600

    Refactoring in ordert o create a generic ParticleSystemHandler script

---
+++ Assets/Scripts/Core/ParticleSystemHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Core
+{
+    public class ParticleSystemHandler : MonoBehaviour
+    {
+        // config
+        [SerializeField] ParticleSystem[] particleSystemsToPlay = null;
+
+        public void PlayParticleSystems()
+        {
+            foreach (var particleSystem in particleSystemsToPlay)
+            {
+                ParticleSystem instance = Instantiate(particleSystem, transform.position, Quaternion.identity);
+                instance.Play();
+            }
+        }
+    }
+}
