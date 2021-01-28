ae053856061d732aadb3b9afa14f9e6d1fffb834
villalbaweb <villalbaweb@gmail.com>
2021-01-28 15:20:00 -0600

    Now the bloodpickups dissapear when taken but appear again afetr a period of time

---
+++ Assets/Scripts/Pickup/PickupDisableController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace LessIsMore.Pickup
+{
+    public class PickupDisableController : MonoBehaviour
+    {
+        // config
+        [SerializeField] float dissapearTime = 10f;
+
+        // cache
+        SpriteRenderer _spriteRenderer;
+        CircleCollider2D _circleCollider2d;
+
+        private void Awake() 
+        {
+            _spriteRenderer = transform.Find("Body").GetComponent<SpriteRenderer>();    
+            _circleCollider2d = GetComponent<CircleCollider2D>();
+        }
+
+        public void PickupDissapear()
+        {
+            StartCoroutine(DissapearCoroutine());
+        }
+
+        IEnumerator DissapearCoroutine()
+        {
+            _spriteRenderer.enabled = false;
+            _circleCollider2d.enabled = false;
+            yield return new WaitForSeconds(dissapearTime);
+            _spriteRenderer.enabled = true;
+            _circleCollider2d.enabled = true;
+        }
+    }
+}
