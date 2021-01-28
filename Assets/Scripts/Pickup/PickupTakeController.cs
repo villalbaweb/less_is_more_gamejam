7f7ff51ae0469ded81ceeceb5227d9fdae1cf53f
villalbaweb <villalbaweb@gmail.com>
2021-01-28 09:10:31 -0600

    Now Blood Pick interacts with players health

--- Assets/Scripts/Pickup/PickupTakeController.cs
+++ Assets/Scripts/Pickup/PickupTakeController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using LessIsMore.Player;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace LessIsMore.Pickup
@@ -6,10 +7,12 @@ namespace LessIsMore.Pickup
     public class PickupTakeController : MonoBehaviour
     {
         // config
+        [SerializeField] int healthPointsToIncrease = 5;
         [SerializeField] UnityEvent onPickup = null;
 
         private void OnTriggerEnter2D(Collider2D other) 
         {
+            other.gameObject.GetComponent<PlayerHealthController>().IncreaseHealth(healthPointsToIncrease);
             onPickup.Invoke();            
         }
     }
