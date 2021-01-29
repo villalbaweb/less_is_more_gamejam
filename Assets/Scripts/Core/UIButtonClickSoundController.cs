c41258eaf6bb59b1a4c84e9dcc96bbfce657a7a0
villalbaweb <villalbaweb@gmail.com>
2021-01-29 08:49:55 -0600

    Adding Click sound to  UI buttons

---
+++ Assets/Scripts/Core/UIButtonClickSoundController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Core
+{
+    public class UIButtonClickSoundController : MonoBehaviour
+    {
+        // config
+        [SerializeField] AudioClip clickSound = null;
+
+        [Range(0, 1)]
+        [SerializeField] float clickSoundVolume = 0.75f;
+
+        // cache
+        GameSoundController _gameSoundController;
+
+        private void Awake()
+        {
+            _gameSoundController = FindObjectOfType<GameSoundController>();
+        }
+
+        public void ClickSoundPlay()
+        {
+            _gameSoundController.PlayClipAtCamera(clickSound, clickSoundVolume);
+        }
+    }
+}
