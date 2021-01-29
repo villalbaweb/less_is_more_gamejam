b2b737ab5e00c1699691992af4b672d65decf03a
villalbaweb <villalbaweb@gmail.com>
2021-01-28 22:21:01 -0600

    Adding transport portals

---
+++ Assets/Scripts/Portal/PortalTeletransporter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Portal
+{
+    public class PortalTeletransporter : MonoBehaviour
+    {
+        // config
+        [SerializeField] Transform spawnPos = null;
+
+        private void OnTriggerEnter2D(Collider2D other) 
+        {
+            other.gameObject.transform.position = spawnPos.position;
+        }
+    }
+}
