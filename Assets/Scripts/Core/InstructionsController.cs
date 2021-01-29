afc83b2f9281ec2369a9bc84b4cf0f9b054b5679
villalbaweb <villalbaweb@gmail.com>
2021-01-29 09:36:51 -0600

    Adding Instructions

---
+++ Assets/Scripts/Core/InstructionsController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Core
+{
+    public class InstructionsController : MonoBehaviour
+    {
+        // state
+        SpriteRenderer _spriteRenderer;
+        Timer _timer;
+
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _timer = GetComponent<Timer>();
+            SubscribeTimerEvents();
+        }
+
+        void SubscribeTimerEvents()
+        {
+            _timer.OnFinished += RemoveTitle;
+            _timer.OnTick += TimerTickAction;
+        }
+
+        void UnsubscribeTimerEvents()
+        {
+            _timer.OnFinished -= RemoveTitle;
+            _timer.OnTick -= TimerTickAction;
+        }
+
+        void RemoveTitle()
+        {
+
+            SetAlpha(0);
+            UnsubscribeTimerEvents();
+        }
+
+        void TimerTickAction()
+        {
+            SetAlpha(_timer.GetTimeLeft() / _timer.GetTotalTime());
+        }
+
+        private void SetAlpha(float value)
+        {
+            Color tmp = _spriteRenderer.color;
+            tmp.a = value;
+            _spriteRenderer.color = tmp;
+        }
+    }
+}
