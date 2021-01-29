000d802b235ecc67dc93db1de7266f2d496aa7e0
villalbaweb <villalbaweb@gmail.com>
2021-01-28 18:15:02 -0600

    Now when player die wits for a period of time and then load main menu

--- Assets/Scripts/Player/PlayerDieHandler.cs
+++ Assets/Scripts/Player/PlayerDieHandler.cs
@@ -1,12 +1,29 @@
 ﻿using UnityEngine;
+using LessIsMore.Core;
+using System.Collections;
 
 namespace LessIsMore.Player
 {
     public class PlayerDieHandler : MonoBehaviour
     {
+        // config
+        [SerializeField] SceneLoader sceneLoader = null;
+        [SerializeField] float loadStartScreenTime = 2f;
+
         public void OnPlayerDead()
         {
-            print("dead...");
+            StartCoroutine(DeadCoroutine());
+        }
+
+        IEnumerator DeadCoroutine()
+        {
+            transform.Find("Body").gameObject.SetActive(false);
+            transform.Find("Aim").gameObject.SetActive(false);
+
+
+            yield return new WaitForSeconds(loadStartScreenTime);
+            
+            sceneLoader.LoadMainMenu();
         }
     }
 }
