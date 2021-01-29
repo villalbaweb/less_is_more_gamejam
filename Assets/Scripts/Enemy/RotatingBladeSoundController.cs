bb4c952cadcbbae787b064a9ed2ee2cf001ab4cf
villalbaweb <villalbaweb@gmail.com>
2021-01-29 08:24:58 -0600

    Adding rotating blade sound on collision

---
+++ Assets/Scripts/Enemy/RotatingBladeSoundController.cs
@@ -0,0 +1,27 @@
+﻿using LessIsMore.Core;
+using UnityEngine;
+
+namespace LessIsMore.Enemy
+{
+    public class RotatingBladeSoundController : MonoBehaviour
+    {
+        // config
+        [SerializeField] AudioClip sawingSound = null;
+
+        [Range(0, 1)]
+        [SerializeField] float sawingSoundVolume = 0.75f;
+
+        // cache
+        GameSoundController _gameSoundController;
+
+        private void Awake()
+        {
+            _gameSoundController = FindObjectOfType<GameSoundController>();
+        }
+        
+        private void OnCollisionEnter2D(Collision2D other) 
+        {
+            _gameSoundController.PlayClipAtCamera(sawingSound, sawingSoundVolume);
+        }
+    }
+}
