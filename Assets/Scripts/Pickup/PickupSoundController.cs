29c27eabe536f2c96975573524a48311fc230cba
villalbaweb <villalbaweb@gmail.com>
2021-01-29 08:36:57 -0600

    Adding pickup sound

---
+++ Assets/Scripts/Pickup/PickupSoundController.cs
@@ -0,0 +1,27 @@
+﻿using LessIsMore.Core;
+using UnityEngine;
+
+namespace LessIsMore.Pickup
+{
+    public class PickupSoundController : MonoBehaviour
+    {
+        // config
+        [SerializeField] AudioClip pickupSound = null;
+
+        [Range(0, 1)]
+        [SerializeField] float pickupSoundVolume = 0.75f;
+
+        // cache
+        GameSoundController _gameSoundController;
+
+        private void Awake()
+        {
+            _gameSoundController = FindObjectOfType<GameSoundController>();
+        }
+
+        public void PickupSoundPlay()
+        {
+            _gameSoundController.PlayClipAtCamera(pickupSound, pickupSoundVolume);
+        }
+    }
+}
