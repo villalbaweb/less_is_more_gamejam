39860ab2d9ce96273526234ef9852780f0e810c1
villalbaweb <villalbaweb@gmail.com>
2021-01-29 07:41:59 -0600

    Adding First Sound

---
+++ Assets/Scripts/Player/PlayerSoundController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using LessIsMore.Core;
+
+namespace LessIsMore.Player
+{
+    public class PlayerSoundController : MonoBehaviour
+    {
+        // config
+        [SerializeField] AudioClip hitSound = null;
+
+        // cache
+        GameSoundController _gameSoundController;
+
+        private void Awake() 
+        {
+            _gameSoundController = FindObjectOfType<GameSoundController>();    
+        }
+
+        public void HurtSoundPlay()
+        {
+            _gameSoundController.PlayClipAtCamera(hitSound);
+        }
+    }
+}
