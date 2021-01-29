48754d2d0625a5fc1c06bf4457e8d8d134edc1a0
villalbaweb <villalbaweb@gmail.com>
2021-01-29 08:59:31 -0600

    Adding game music (wind) sound

---
+++ Assets/Scripts/Core/GameMusic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace LessIsMore.Core
+{
+    public class GameMusic : MonoBehaviour
+    {
+        // config props
+        [SerializeField] AudioClip gameMusic = null;
+
+        // cache
+        AudioSource _musicAudioSource;
+
+        // Start is called before the first frame update
+        void Awake()
+        {
+            _musicAudioSource = GetComponent<AudioSource>();
+
+            PlayRegularGameMusic();
+        }
+
+        public void PlayRegularGameMusic()
+        {
+            _musicAudioSource.Stop();
+            _musicAudioSource.clip = gameMusic;
+            _musicAudioSource.Play();
+        }
+    }
+}
